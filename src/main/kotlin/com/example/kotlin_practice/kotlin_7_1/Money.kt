16b6f6400313e88760975af656efc67b8b1a23e8
icarus8050 <icarus8050@naver.com>
2022-08-23 00:48:13 +0900

    오버로딩

--- src/main/kotlin/com/example/kotlin_practice/kotlin_7_1/Money.kt
+++ src/main/kotlin/com/example/kotlin_practice/kotlin_7_1/Money.kt
@@ -19,6 +19,8 @@ data class Money(
     operator fun times(other: Money) = Money(value * other.value)
     operator fun div(other: Money) = Money(value / other.value)
     operator fun rem(other: Money) = Money(value % other.value)
+
+    operator fun plus(other: Long) = Money(value + other)
 }
 
 fun main() {
@@ -27,4 +29,6 @@ fun main() {
     println(Money.of(10) * Money.of(20)) // 200
     println(Money.of(500) / Money.of(10)) // 50
     println(Money.of(5) % Money.of(3)) // 2
+
+    println(Money.of(100) + 500) // 600
 }
