d49dafc82ac6eef7e6e6074358ef94a569e648dd
icarus8050 <icarus8050@naver.com>
2022-09-21 01:30:40 +0900

    폴더블 이진 트리에 flodMap 함수 만들기

---
+++ src/main/kotlin/com/example/kotlin_practice/functional9/Foldable.kt
@@ -0,0 +1,41 @@
+package com.example.kotlin_practice.functional9
+
+interface Foldable<out A> {
+    fun <B> foldLeft(acc: B, f: (B, A) -> B): B
+    fun <B> foldMap(f: (A) -> B, m: Monoid<B>): B = foldLeft(m.mempty()) { b, a ->
+        m.mappend(b, f(a))
+    }
+}
+
+sealed class BinaryTree<out A> : Foldable<A> {
+    override fun <B> foldLeft(acc: B, f: (B, A) -> B): B = when(this) {
+        is EmptyTree -> acc
+        is Node -> {
+            val leftAcc = leftTree.foldLeft(acc, f)
+            val rootAcc = f(leftAcc, value)
+            rightTree.foldLeft(rootAcc, f)
+        }
+    }
+}
+
+data class Node<A>(
+    val value: A,
+    val leftTree: BinaryTree<A> = EmptyTree,
+    val rightTree: BinaryTree<A> = EmptyTree,
+) : BinaryTree<A>()
+
+object EmptyTree : BinaryTree<Nothing>()
+
+fun main() {
+    val tree = Node(1,
+        Node(2,
+            Node(3), Node(4)),
+        Node(5,
+            Node(6), Node(7)))
+
+    println(tree.foldLeft(0) { a, b -> a + b}) // 28
+    println(tree.foldLeft(1) { a, b -> a * b}) // 5040
+
+    println(tree.foldMap({ a -> a * 2}, SumMonoid())) // 56
+    println(tree.foldMap({ a -> a + 1}, ProductMonoid())) // 40320
+}
